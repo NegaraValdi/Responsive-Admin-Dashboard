9f3d703c1aa4145b8929fa556b0d9c5dbf880a1b
NegaraValdi <123758446+NegaraValdi@users.noreply.github.com>
2023-12-13 20:46:46 +0700

    Responsive Admin Dashboard

---
+++ orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+    {
+        productName:'Foldable Mini Drone',
+        productNumber: '89564',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+
+    {
+        productName:'DJI Mavic Pro 2',
+        productNumber:'487239',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+
+    {
+        productName: 'DJI Phantom Mini',
+        productNumber: '472978',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Autel Robotics Evo',
+        productNumber: '172859',
+        paymentStatus: 'Paid',
+        shipping:'Delivered'
+    },
+
+    {
+        productName: 'Skydio 2 Pro',
+        productNumber: '92741',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'Parrot Anafi',
+        productNumber: '62842',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Yuneec Typhoon 2',
+        productNumber: '20482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
